26cf433a0fdefa2ab3ff91135365b85125e6a081
Nathan Fisher <N.Fisher21@outlook.com>
2024-08-09 15:36:35 +0000

    add login() to routes.py and login.html. login.html tested and works login() is yet to be tested once register() is created and working

--- starwars_blog/models.py
+++ starwars_blog/models.py
@@ -8,22 +8,29 @@ from starwars_blog import db, login
 class User(UserMixin, db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), index=True, unique=True,)
+    username = db.Column(db.String(50), index=True, unique=True, nullable=False)
     email = db.Column(db.String(100), index=True, unique=True, nullable=True)
     password_hash = db.Column(db.String(50))
 
     def set_password(self, password):
+        # This is to set the user's password hash
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
+        # This will check if the provided password matches the stored password hash
         return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
     
     
+# This will load the user by their user ID
 @login.user_loader(id)
 def load_user(id):
     return User.query.get(int(id))
 
 
+# This class is for the post creation layout
 class Post (db.Model):
     __tablename__ = "post"
     id = db.Column(db.Integer, primary_key=True)
@@ -36,7 +43,8 @@ class Post (db.Model):
     year_of_release = db.Column(db.Integer, nullable=True)
     favourite_character = db.Column(db.String(100), nullable=True)
     
-    
+
+# This class is for the comment creation layout
 class Comment(db.Model):
     __tablename__ = "comment"
     id = db.Column(db.Integer, primary_key=True)
--- starwars_blog/routes.py
+++ starwars_blog/routes.py
@@ -1,28 +1,30 @@
 from flask import render_template, flash, redirect, url_for, request, current_app as app
-from flask_login import current_user, login_user, logout_user, login_required
+from flask_login import current_user, login_user, logout_user, login_required, LoginManager
 from starwars_blog import db
 from starwars_blog.models import User, Post, Comment
 from urllib.parse import urlparse as url_parse
 
 
 
-@app.route("/")
-@app.route("/index")
+@app.route('/')
+@app.route('/index')
 def index():
     posts = Post.query.all()
-    return render_template("index.html", )
+    return render_template('index.html', title='Home', posts=posts)
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/login', methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for("index"))
+        return redirect(url_for('index'))
     if request.method == "POST":
-        username = request.form["username"]
+        login_input = request.form["username"]
         password = request.form["password"]
-        user = User.query.filter_by(username=username).first()
+        user = User.query.filter(
+            (User.username == login_input) | (User.email == login_input)
+        ).first()
         if user is None or not user.check_password(password):
             flash("Invalid username or password")
-            return redirect(url_for("login"))
+            return redirect(url_for('login'))
         login_user(user, remember=request.form.get("remember_me"))
-        return redirect(url_for("index"))
+    return render_template('login.html', title='Sign In')
         
